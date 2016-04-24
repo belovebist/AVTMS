649183a817290b78a602ef2730dc4cf76f8b9fb7
belovebist <belovebist@gmail.com>
2016-04-24 22:18:54 +0545

    checking the repo

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+This is the main script that starts the program.
+It handles all the initializations and starts-up 
+the program.
+"""
+
+
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
